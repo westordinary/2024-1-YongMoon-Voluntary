036a9683500cd714adabaff5b7a631b24d614e67
Choi-GeonUng <56158371+ddugel3@users.noreply.github.com>
2024-07-31 19:11:08 +0900

    2일차 JS 연산자 실습 코드

---
+++ 2일차/test.js
@@ -0,0 +1,40 @@
+var x = 10, y = 4;
+
+console.log(x + y);
+console.log(x - y); 
+console.log(x * y); 
+console.log(x / y); 
+console.log(x % y);          
+
+var x = 10, y = 10, z = 10;
+x = x - 5;
+y -= 5;
+z =- 5;
+
+var x = 10;
+var y = x-- + 5 + --x;
+
+var x = 3, y = '3', z = 3;
+console.log((x == y));  
+console.log((x === y)); 
+console.log(x === z);
+
+
+var x = 3, y = 5;
+var a = "abc", b = "bcd";
+console.log((x > y)); 
+console.log((a <= b));
+console.log(x < a);
+
+
+var x = 3, y = '3', z = 3;
+console.log((x == y)); 
+console.log((x === y));
+console.log(x === z);     
+
+
+var x = 15, y = 8, z = 15;
+console.log((x << 1));
+console.log((y >> 1));
+
+
